429e21f428878720642bbd1543b3163c40409797
mkazic1 <mkazic1@etf.unsa.ba>
2020-11-06 19:23:12 +0100

    Metode getteri i setteri u klasi Inerval

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,29 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double pocetna_tacka;
+    private double krajnja_tacka;
+
+    public double getPocetna_tacka() {
+        return pocetna_tacka;
+    }
+
+    public void setPocetna_tacka(double pocetna_tacka) {
+        this.pocetna_tacka = pocetna_tacka;
+    }
+
+    public double getKrajnja_tacka() {
+        return krajnja_tacka;
+    }
+
+    public void setKrajnja_tacka(double krajnja_tacka) {
+        this.krajnja_tacka = krajnja_tacka;
+    }
+
+    /*public boolean isIn(double v) {
+    }
 
 
+    public boolean isNull() {*/
+    }
 }
